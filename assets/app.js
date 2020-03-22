6d5180f09906c93f3a3e774c2e2f0210b66ab3ba
Laurens Debackere <laurens.debackere@gmail.com>
2020-03-22 18:54:26 +0100

    Still WIP, added google sheets integration

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,30 @@
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
 $(document).ready(function () {
 	if (
 		location.protocol !== "https:" &&
@@ -9,7 +36,15 @@ $(document).ready(function () {
 			`https:${location.href.substring(location.protocol.length)}`
 		);
 	}
+	var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1TvDQjbYOaIBCXUpN8g0Fa6rU3YCxBiBDQM5EtD9Tlv8/edit#gid=0';
+	var HRTemplate = Handlebars.compile($('#hr-template').html());
 
+	// Load top five HR leaders.
+	$('#hr').sheetrock({
+	url: mySpreadsheet,
+	query: "select A,B,C,D",
+	rowTemplate: HRTemplate
+	});
 });
 
 
